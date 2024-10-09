945a048edb12ad2fa733151ccece3d9904d7056c
rn0x7f <ampheriz0@gmail.com>
2024-10-08 19:58:54 -0600

    add: doctor, patient routers

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,10 @@
 # author: Renato García Morán
 from models import * # Crea las tablas en la base de datos
+from predictions import * # Descarga el modelo preentrenado
 from fastapi import FastAPI
+from routes import doctor
+from routes import patient
 
 app = FastAPI()
+app.include_router(doctor.doctor, prefix='/api/doctors', tags=['Doctor'])
+app.include_router(patient.patient, prefix='/api/patients', tags=['Patient'])
