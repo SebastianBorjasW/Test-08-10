e2508cdfa03086d000b63839b6b705c74b935626
rn0x7f <ampheriz0@gmail.com>
2024-10-09 12:18:06 -0600

    init: app

---
+++ frontend/dr_app/lib/main.dart
@@ -0,0 +1,20 @@
+// author: Renato García Morán
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dr App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
