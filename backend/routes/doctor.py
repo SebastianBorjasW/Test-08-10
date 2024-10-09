129fc3cddacfa53f8c9e201fd3f70bc1db3bbd55
rn0x7f <ampheriz0@gmail.com>
2024-10-08 19:52:50 -0600

    add: doctor route / create_doctor / get_doctors / delete_doctor

---
+++ backend/routes/doctor.py
@@ -0,0 +1,33 @@
+# author: Renato García Morán
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from schemas.doctor import Doctor, DoctorCreate
+from utils import doctor_crud
+from config.config import Config
+
+doctor = APIRouter()
+
+@doctor.post('/')
+def create_doctor(doctor_create: DoctorCreate, db: Session = Depends(Config.get_db)):
+    ok = doctor_crud.create_doctor(db, doctor_create)
+    message = ''
+    if ok:
+        message = 'OK'
+    else:
+        message = 'ERROR'
+    return {'message': message}
+
+@doctor.get('/', response_model=list[Doctor])
+def get_doctors(skip: int = 0, limit = 100, db: Session = Depends(Config.get_db)):
+    return doctor_crud.get_doctors(db, skip=skip, limit=limit)
+
+
+@doctor.delete('/{doctor_id}')
+def delete_doctor(doctor_id: int, db: Session = Depends(Config.get_db)):
+    ok = doctor_crud.delete_doctor(db, doctor_id)
+    message = ''
+    if ok:
+        message = 'OK'
+    else:
+        message = 'ERROR'
+    return {'message': message}
