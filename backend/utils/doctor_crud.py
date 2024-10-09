9516c43b4a1441cf39e2bd192f512e9320413f3c
rn0x7f <ampheriz0@gmail.com>
2024-10-08 19:53:34 -0600

    add: doctor_crud -> create, read, delete

---
+++ backend/utils/doctor_crud.py
@@ -0,0 +1,41 @@
+# author: Renato García Morán
+from sqlalchemy.orm import Session
+from fastapi import HTTPException
+import models
+import schemas
+
+# Obtener doctores
+def get_doctors(db: Session, skip: int = 0, limit: int = 100):
+    """Obtiene todos los doctores en la db."""
+    return db.query(models.Doctor).offset(skip).limit(limit).all()
+
+# Crea un doctor
+def create_doctor(db: Session, doctor_create: schemas.doctor.DoctorCreate):
+    """Crea un doctor en la db."""
+    doctor = models.Doctor(
+        first_name=doctor_create.first_name,
+        last_name=doctor_create.last_name,
+        email=doctor_create.email,
+        password=doctor_create.password
+    )
+
+    if get_doctor_by_email(db, doctor_create.email):
+        raise HTTPException(status_code=400, detail="El email ya está registrado.")
+
+    db.add(doctor)
+    db.commit()
+    db.refresh(doctor)
+    return True
+    
+def get_doctor_by_email(db: Session, email: str):
+    """Obtiene un doctor por su email."""
+    return db.query(models.Doctor).filter(models.Doctor.email == email).first()
+
+def delete_doctor(db: Session, doctor_id: int):
+    """Elimina un doctor por su id."""
+    doctor = db.query(models.Doctor).filter(models.Doctor.id == doctor_id).first()
+    if doctor is None:
+        raise HTTPException(status_code=404, detail="Doctor no encontrado.")
+    db.delete(doctor)
+    db.commit()
+    return True
