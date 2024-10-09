865ac95f817516eedfb96a8c19913259eacdf92b
rn0x7f <ampheriz0@gmail.com>
2024-10-09 07:21:19 -0600

    add: auth router

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,9 @@ from predictions import * # Descarga el modelo preentrenado
 from fastapi import FastAPI
 from routes import doctor
 from routes import patient
+from routes import auth
 
 app = FastAPI()
 app.include_router(doctor.doctor, prefix='/api/doctors', tags=['Doctor'])
+app.include_router(auth.auth, prefix='/api/auth/doctor', tags=['Auth'])
 app.include_router(patient.patient, prefix='/api/patients', tags=['Patient'])
