c37fb9214d5c002dccd34bc35701f2cf6df5ef69
rn0x7f <ampheriz0@gmail.com>
2024-10-10 08:24:50 -0600

    update: from doctor id to doctor name

--- frontend/dr_app/lib/screens/dashboard_screen.dart
+++ frontend/dr_app/lib/screens/dashboard_screen.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'add_patient_screen.dart';
 import '../services/api_service.dart';
+import '../models/patient_model.dart';
+import 'patient_detail_screen.dart';
 
 class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({super.key});
+
   @override
   _DashboardScreenState createState() => _DashboardScreenState();
 }
@@ -12,6 +16,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   final ApiService apiService = ApiService();
   List<Patient> patients = [];
   bool isLoading = true;
+  Map<int, String> doctorNames = {}; // almacenar los nombres de los doctores
 
   @override
   void initState() {
@@ -26,6 +31,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
         patients = response;
         isLoading = false;
       });
+      await fetchDoctorNames(); // Obtener los nombres de los doctores
     } catch (e) {
       setState(() {
         isLoading = false;
@@ -36,87 +42,165 @@ class _DashboardScreenState extends State<DashboardScreen> {
     }
   }
 
+  Future<void> fetchDoctorNames() async {
+    for (var patient in patients) {
+      if (!doctorNames.containsKey(patient.doctor_id)) {
+        // si el ID del doctor no está en el map, se obtiene con la api
+        try {
+          final doctor = await apiService.getDoctor(patient.doctor_id);
+          if (doctor != null) {
+            setState(() {
+              doctorNames[patient.doctor_id] = doctor.first_name;
+            });
+          }
+        } catch (e) {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text(
+                'Error al obtener el doctor con ID ${patient.doctor_id}: $e'),
+          ));
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[850],
-      appBar: AppBar(
-        title: Text(
-          'Dashboard',
-          style: TextStyle(color: Colors.white),
+        backgroundColor: Colors.grey[850],
+        appBar: AppBar(
+          title: const Text(
+            'Dashboard',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.grey[900],
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Center(
+                child: Text(
+                  'Pacientes',
+                  style: TextStyle(color: Colors.grey[600]),
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) =>
+                        AddPatientScreen(),
+                    transitionDuration: Duration.zero,
+                    reverseTransitionDuration: Duration.zero,
+                  ),
+                );
+              },
+              child: const Text(
+                'Registrar Paciente',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginScreen()),
+                );
+              },
+              child: const Text(
+                'Cerrar Sesión',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
         ),
-        backgroundColor: Colors.grey[900],
-        actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Center(
-              child: Text(
-                'Pacientes',
-                style: TextStyle(color: Colors.grey[600]),
-              ),
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (context, animation, secondaryAnimation) =>
-                      AddPatientScreen(),
-                  transitionDuration: Duration.zero,
-                  reverseTransitionDuration: Duration.zero,
+        body: isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: SizedBox(
+                  width: double.infinity,
+                  child: DataTable(
+                    columns: const [
+                      DataColumn(
+                          label: Expanded(
+                              child: Center(
+                                  child: Text('Nombre',
+                                      style: TextStyle(color: Colors.white))))),
+                      DataColumn(
+                          label: Expanded(
+                              child: Center(
+                                  child: Text('Apellido',
+                                      style: TextStyle(color: Colors.white))))),
+                      DataColumn(
+                          label: Expanded(
+                              child: Center(
+                                  child: Text('Sexo',
+                                      style: TextStyle(color: Colors.white))))),
+                      DataColumn(
+                          label: Expanded(
+                              child: Center(
+                                  child: Text('Fecha Nac.',
+                                      style: TextStyle(color: Colors.white))))),
+                      DataColumn(
+                          label: Expanded(
+                              child: Center(
+                                  child: Text('ID Doctor',
+                                      style: TextStyle(color: Colors.white))))),
+                      DataColumn(
+                          label: Expanded(
+                              child: Center(
+                                  child: Text('Diagnóstico',
+                                      style: TextStyle(color: Colors.white))))),
+                    ],
+                    rows: patients.map((patient) {
+                      final doctorName =
+                          doctorNames[patient.doctor_id] ?? 'Desconocido';
+                      return DataRow(
+                        cells: [
+                          DataCell(Center(
+                              child: Text(patient.first_name,
+                                  style:
+                                      const TextStyle(color: Colors.white)))),
+                          DataCell(Center(
+                              child: Text(patient.last_name,
+                                  style:
+                                      const TextStyle(color: Colors.white)))),
+                          DataCell(Center(
+                              child: Text(patient.sex,
+                                  style:
+                                      const TextStyle(color: Colors.white)))),
+                          DataCell(Center(
+                              child: Text(
+                                  patient.dob
+                                      .toIso8601String()
+                                      .substring(0, 10),
+                                  style:
+                                      const TextStyle(color: Colors.white)))),
+                          DataCell(Center(
+                              child: Text(doctorName,
+                                  style:
+                                      const TextStyle(color: Colors.white)))),
+                          DataCell(Center(
+                              child: Text(patient.diagnosis,
+                                  style:
+                                      const TextStyle(color: Colors.white)))),
+                        ],
+                        onSelectChanged: (selected) {
+                          if (selected ?? false) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    PatientDetailScreen(patient: patient),
+                              ),
+                            );
+                          }
+                        },
+                      );
+                    }).toList(),
+                  ),
                 ),
-              );
-            },
-            child: Text(
-              'Registrar Paciente',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => LoginScreen()),
-              );
-            },
-            child: Text(
-              'Cerrar Sesión',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        ],
-      ),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Container(
-                width: double.infinity,
-                child: DataTable(
-                  columns: [
-                    DataColumn(label: Expanded(child: Center(child: Text('Nombre', style: TextStyle(color: Colors.white))))),
-                    DataColumn(label: Expanded(child: Center(child: Text('Apellido', style: TextStyle(color: Colors.white))))),
-                    DataColumn(label: Expanded(child: Center(child: Text('Sexo', style: TextStyle(color: Colors.white))))),
-                    DataColumn(label: Expanded(child: Center(child: Text('Fecha Nac.', style: TextStyle(color: Colors.white))))),
-                    DataColumn(label: Expanded(child: Center(child: Text('ID Doctor', style: TextStyle(color: Colors.white))))),
-                    DataColumn(label: Expanded(child: Center(child: Text('Diagnóstico', style: TextStyle(color: Colors.white))))),
-                  ],
-                  rows: patients
-                      .map(
-                        (patient) => DataRow(cells: [
-                          DataCell(Center(child: Text(patient.first_name, style: TextStyle(color: Colors.white)))),
-                          DataCell(Center(child: Text(patient.last_name, style: TextStyle(color: Colors.white)))),
-                          DataCell(Center(child: Text(patient.sex, style: TextStyle(color: Colors.white)))),
-                          DataCell(Center(child: Text(patient.dob.toIso8601String().substring(0, 10), style: TextStyle(color: Colors.white)))),
-                          DataCell(Center(child: Text(patient.doctor_id.toString(), style: TextStyle(color: Colors.white)))),
-                          DataCell(Center(child: Text(patient.diagnosis, style: TextStyle(color: Colors.white)))),
-                        ]),
-                      )
-                      .toList(),
-                ),
-              ),
-            ),
-    );
+              ));
   }
 }
