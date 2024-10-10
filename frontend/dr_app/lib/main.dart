95f2e27079646f7399f01d6154d1aaf11f554560
rn0x7f <ampheriz0@gmail.com>
2024-10-10 10:02:10 -0600

    update: color

--- frontend/dr_app/lib/main.dart
+++ frontend/dr_app/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Dr App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       home: LoginScreen(),
     );
