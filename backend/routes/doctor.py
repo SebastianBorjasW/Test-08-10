adbc872aa9ccc5b8244b0650983cb0dc09e77e46
rn0x7f <ampheriz0@gmail.com>
2024-10-10 22:36:40 -0600

    add: get doctor by email route

--- backend/routes/doctor.py
+++ backend/routes/doctor.py
@@ -1,5 +1,5 @@
 # author: Renato García Morán
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from schemas.doctor import Doctor, DoctorCreate
 from utils import doctor_crud
@@ -24,6 +24,13 @@ def get_doctors(skip: int = 0, limit = 100, db: Session = Depends(Config.get_db)
 @doctor.get('/{doctor_id}', response_model=Doctor)
 def get_doctor(doctor_id: int, db: Session = Depends(Config.get_db)):
     return doctor_crud.get_doctor(db, doctor_id)
+
+@doctor.get('/email/{email}', response_model=Doctor)
+def get_doctor_by_email(email: str, db: Session = Depends(Config.get_db)):
+    doctor = doctor_crud.get_doctor_by_email(db, email)
+    if doctor is None:
+        raise HTTPException(status_code=404, detail="Doctor no encontrado.")
+    return doctor
 
 @doctor.delete('/{doctor_id}')
 def delete_doctor(doctor_id: int, db: Session = Depends(Config.get_db)):
